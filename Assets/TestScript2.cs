5e7d541921ba15cf5c6cce5ee5c3138fa9662d9f
cnlohr <lohr85@gmail.com>
2025-06-02 02:54:10 -0700

    Working with generics

--- Assets/TestScript2.cs
+++ Assets/TestScript2.cs
@@ -6,7 +6,11 @@ public class TestScript2 : MonoBehaviour
     void Update() {	
 		MaterialPropertyBlock block = new MaterialPropertyBlock();
 		MeshRenderer mr = GetComponent<MeshRenderer>();
-		block.SetVector( "_Color", new Vector4( Mathf.Sin( (float)Time.time )*0.5f+0.5f, 0.5f, 0, 1 ) );
+		block.SetVector( "_Color",
+			new Vector4(
+				Mathf.Sin( (float)Time.time * 5.0f )*0.5f+0.5f,
+				0.5f, 0, 1 ) );
 		mr.SetPropertyBlock(block);
+		Debug.Log( block );
 	}
 }
