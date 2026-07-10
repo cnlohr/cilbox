92c0c0a2d97331379aca3a580760507468ead38e
BoatFloater <111416210+BoatFloater@users.noreply.github.com>
2026-07-10 13:03:30 +0900

    Typed Struct Migration: Method

--- Packages/com.cnlohr.cilbox/CilboxSerialization.cs
+++ Packages/com.cnlohr.cilbox/CilboxSerialization.cs
@@ -147,6 +147,105 @@ namespace Cilbox
 		}
 	}
 
+	public class SerializedMethod
+	{
+		public string methodName;
+		public int maxStack;
+		public bool isVoid;
+		public bool isStatic;
+		public bool isCtor;
+		public string fullSignature;
+		public byte[] body;
+		public SerializedField[] locals;
+		public SerializedField[] parameters;
+		public SerializedExceptionHandler[] exceptionHandlers;
+
+		// for legacy serializations, methodName is the enclosing Map key, not part of this map.
+		// Master MethodProps order: body, [eh], locals, parameters, maxStack,
+		// isVoid, isCtor, isStatic, name, fullSignature.
+		public Serializee ToSerializee()
+		{
+			Dictionary<String, Serializee> ret = new Dictionary<String, Serializee>();
+			ret["body"] = Serializee.CreateFromBlob( body );
+
+			if( exceptionHandlers != null && exceptionHandlers.Length > 0 )
+			{
+				Serializee[] eh = new Serializee[exceptionHandlers.Length];
+				for( int i = 0; i < exceptionHandlers.Length; i++ )
+					eh[i] = exceptionHandlers[i].ToSerializee();
+				ret["eh"] = new Serializee( eh );
+			}
+
+			Serializee[] loc = new Serializee[locals.Length];
+			for( int i = 0; i < locals.Length; i++ )
+				loc[i] = locals[i].ToMethodFieldSerializee();
+			ret["locals"] = new Serializee( loc );
+
+			Serializee[] par = new Serializee[parameters.Length];
+			for( int i = 0; i < parameters.Length; i++ )
+				par[i] = parameters[i].ToMethodFieldSerializee();
+			ret["parameters"] = new Serializee( par );
+
+			ret["maxStack"] = new Serializee( maxStack.ToString() );
+			ret["isVoid"] = new Serializee( isVoid ? "1" : "0" );
+			ret["isCtor"] = new Serializee( isCtor ? "1" : "0" );
+			ret["isStatic"] = new Serializee( isStatic ? "1" : "0" );
+			ret["name"] = new Serializee( methodName );
+			ret["fullSignature"] = new Serializee( fullSignature );
+			return new Serializee( ret );
+		}
+
+		public static SerializedMethod FromSerializee( Serializee s, String methodName )
+		{
+			Dictionary<String, Serializee> m = s.AsMap();
+			SerializedMethod sm = new SerializedMethod();
+			sm.methodName = methodName;
+			sm.body = m["body"].AsBlob();
+
+			if( m.TryGetValue( "eh", out Serializee ehS ) )
+			{
+				Serializee[] eh = ehS.AsArray();
+				sm.exceptionHandlers = new SerializedExceptionHandler[eh.Length];
+				for( int i = 0; i < eh.Length; i++ )
+					sm.exceptionHandlers[i] = SerializedExceptionHandler.FromSerializee( eh[i] );
+			}
+			else
+			{
+				sm.exceptionHandlers = new SerializedExceptionHandler[0];
+			}
+
+			Serializee[] loc = m["locals"].AsArray();
+			sm.locals = new SerializedField[loc.Length];
+			for( int i = 0; i < loc.Length; i++ )
+				sm.locals[i] = SerializedField.FromMethodFieldSerializee( loc[i] );
+
+			Serializee[] par = m["parameters"].AsArray();
+			sm.parameters = new SerializedField[par.Length];
+			for( int i = 0; i < par.Length; i++ )
+				sm.parameters[i] = SerializedField.FromMethodFieldSerializee( par[i] );
+
+			if( m.TryGetValue("isCtor", out Serializee isCtorSer) )
+			{
+				sm.isCtor = Convert.ToInt32(isCtorSer.AsString()) != 0;
+			}
+			else
+			{
+				// Backward compatibility for payloads generated before isCtor existed.
+				sm.isCtor = methodName == ".ctor" || methodName == ".cctor" || sm.fullSignature.StartsWith("Void .ctor(") || sm.fullSignature.StartsWith("Void .cctor(");
+			}
+
+			sm.maxStack = Int32.Parse( m["maxStack"].AsString() );
+			sm.isVoid = Int32.Parse( m["isVoid"].AsString() ) != 0;
+			sm.isStatic = Int32.Parse( m["isStatic"].AsString() ) != 0;
+			sm.fullSignature = m["fullSignature"].AsString();
+			if (m.TryGetValue( "name", out Serializee nameS))
+			{
+				sm.methodName = nameS.AsString();
+			}
+			return sm;
+		}
+	}
+
 	/// <summary>
 	/// Helper to build a SerializedTypeDescriptor from a native System.Type.
 	/// Used by the editor compiler to build type descriptors from native System.Type.
